ce8f330147eb8f4bb9300d8f7f7bec83d46e8495
Shenia23 <eugeku13@gmail.com>
2020-10-01 11:52:51 +0200

    able to send files

--- Client/client.py
+++ Client/client.py
@@ -1,25 +1,40 @@
 import socket
 
-SERVER_IP = '192.168.1.98'
-SERVER_PORT = 12345
+SERVER_IP = '127.0.0.1'
+SERVER_PORT = 12340
 MESSAGE = b'Hello Server!'
 BUFFER_SIZE = 1024
 
 def main():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((SERVER_IP,SERVER_PORT))
-    print('Sending data: {}'.format(MESSAGE))
-    client.send(MESSAGE)
+    print('Sending data: ')
+
+  #while True:
+    f = open("myfile.txt", "rb")
+    content = f.read(1024)
+    
+    while content:
+        # Enviar contenido.
+        client.send(content)
+        content = f.read(1024)
+    
+    print('File sent')
+
+    #client.send(MESSAGE)
     data = client.recv(BUFFER_SIZE)
 
     client.close()
+    f.close()
 
-<<<<<<< HEAD
-print("The server responded: {}".format(data))
-=======
     print('The server responded: {}'.format(data))
 
+def generate_file():
+    print('Generación de un fichero para probar el envio de datos en conexión WiFi')
+    file = open("prueba.txt", "w")
+    file.write("Para comprobar el correcto funcionamiento de la conexión")
+    file.close()
+        
 
 if __name__ == '__main__':
     main()
->>>>>>> a4b5185c49d32248e9dc762621ea417256cf7343
--- Server/server.py
+++ Server/server.py
@@ -1,7 +1,7 @@
 import socket
 
-SERVER_IP = '192.168.1.98'
-SERVER_PORT = 12345
+SERVER_IP = '127.0.0.1'
+SERVER_PORT = 12340
 MESSAGE = b'Hello Client!'
 BUFFER_SIZE = 1024
 
@@ -15,13 +15,27 @@ def main():
 
         conn, addr = server.accept()
         print('Connection address:', addr)
-        while True:
-            data = conn.recv(BUFFER_SIZE)
-            if not data: 
-                break
-            print('Received data:', data)
-            conn.send(MESSAGE)
 
+        conn.setblocking(0)
+
+
+        f = open("recibido.txt", "wb")
+
+        while (True):
+
+    # Recibimos y escribimos en el fichero
+            try:
+                input_data = conn.recv(1024)
+                f.write(input_data)
+            except BlockingIOError:
+                break
+
+        f.close()
+
+        
+        print("El archivo se ha recibido correctamente.")
+
+        conn.send(b'Fichero recibido')
         conn.close()
 
 if __name__ == '__main__':
